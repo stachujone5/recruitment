bcde6ee0df418de744a237ae68ca769114bcf370
Stanisław Jakubowski <sjakubowski3554@gmail.com>
2023-09-11 19:17:01 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,161 @@
-import Image from 'next/image'
+import {
+  ChevronLeft,
+  Hand,
+  Heart,
+  Lock,
+  Menu,
+  PenSquare,
+  ShoppingCart,
+  User2,
+} from "lucide-react";
+import Image from "next/image";
+import background from "./background.png";
+import logo from "./logo.svg";
+import human from "./human.svg";
+import skull from "./skull.svg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="w-[400px] border px-4 py-8 relative">
+      <Image
+        src={background}
+        alt=""
+        className="absolute top-0 left-0 right-0 -z-10"
+      />
+
+      {/* Should be changed to links */}
+      <nav className="flex items-center justify-between">
+        <button>
+          <Menu size={25} className="text-[#172B4D]" />
+        </button>
+        <div className="flex items-center justify-end gap-4">
+          <button>
+            <Heart className="text-black" size={30} />
+          </button>
+          <button>
+            <ShoppingCart className="text-black" size={30} />
+          </button>
+          <button>
+            <User2 className="text-black" size={30} />
+          </button>
         </div>
-      </div>
+      </nav>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <main className="flex flex-col gap-4 justify-center">
+        <Image src={logo} alt="" className="mx-auto my-4" />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <h2 className="text-[#141414] text-center text-[1.3125rem]">
+          Generate your ART by typing in field below any words that comes to
+          your mind!
+        </h2>
+
+        <div className="grid grid-cols-3">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <div className="bg-[#CE2B28] px-2 w-[30px] rounded-[0.5rem] flex items-center justify-center relative text-white h-[30px]">
+              1
+              <div className="absolute top-1/2 -translate-y-1/2 left-0 w-[90px] bg-[#CE2B28] rounded-[0.5rem] h-[3px] -z-10" />
+              <div className="absolute top-1/2 -translate-y-1/2 right-0 w-[75px] bg-[#CE2B28] rounded-[0.5rem] h-[3px] -z-10" />
+            </div>
+            <p className="text-[#CE2B28] text-[0.70rem] font-semibold text-center">
+              Generate image
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <div className="bg-[#9C9C9C] px-2 w-[30px] rounded-[0.5rem] flex items-center justify-center relative text-white h-[30px]">
+              2
+              <div className="absolute top-1/2 -translate-y-1/2 left-0 w-[90px] bg-[#E5E5E7] rounded-[0.5rem] h-[3px] -z-10" />
+              <div className="absolute top-1/2 -translate-y-1/2 right-0 w-[90px] bg-[#E5E5E7] rounded-[0.5rem] h-[3px] -z-10" />
+            </div>
+            <p className="text-[#E5E5E7] text-[0.70rem] font-semibold text-center">
+              Choose your product
+            </p>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2">
+            <div className="bg-[#9C9C9C] px-2 w-[30px] rounded-[0.5rem] flex items-center justify-center relative text-white h-[30px]">
+              3
+              <div className="absolute top-1/2 -translate-y-1/2 left-0 w-[75px] bg-[#E5E5E7] rounded-[0.5rem] h-[3px] -z-10" />
+              <div className="absolute top-1/2 -translate-y-1/2 right-0 w-[90px] bg-[#E5E5E7] rounded-[0.5rem] h-[3px] -z-10" />
+            </div>
+            <p className="text-[#E5E5E7] text-[0.70rem] font-semibold text-center">
+              Order
+            </p>
+          </div>
+        </div>
+
+        <div className="flex justify-between">
+          <input
+            className="w-[80%] rounded-[0.5625rem] bg-[#F5F5F6] px-2 shadow-md placeholder:text-[#C0C1C9] text-[1.0625rem]"
+            placeholder="What do you want to create?"
+          />
+          <button className="w-[20%] bg-[#CB2B28] p-2 flex items-center justify-center rounded-[0.4375rem]">
+            <PenSquare className="text-white" size={30} />
+          </button>
+        </div>
+
+        <div className="flex gap-4 items-center justify-center">
+          <button className="rounded-[0.4375rem] bg-[#F5F5F6] px-4 py-2 text-black text-[0.875rem] font-semibold">
+            High Quality
+          </button>
+          <button className="rounded-[0.4375rem] bg-[#F5F5F6] px-4 py-2 text-black text-[0.875rem] font-semibold">
+            Pop Art
+          </button>
+          <button className="rounded-[0.4375rem] bg-[#F5F5F6] px-4 py-2 text-black text-[0.875rem] font-semibold">
+            Realistic Look
+          </button>
+        </div>
+
+        <div className="p-4 rounded-[0.5625rem] flex bg-[#E3E4E7] bg-opacity-[22%]">
+          <div className="flex flex-col w-1/2">
+            <div className="flex gap-2 items-center">
+              <button>
+                <ChevronLeft className="text-black" size={30} />
+              </button>
+              <p> Style preview </p>
+            </div>
+            <Image alt="Generated image" src={human} />
+          </div>
+
+          <div className="flex gap-2">
+            <div className="flex items-center justify-center">
+              <Hand size={30} className="text-black" />
+            </div>
+            <div className="flex flex-col items-center justify-center gap-4">
+              <p className="uppercase text-black text-[0.875rem]">
+                minimalistic
+              </p>
+              <button className="text-[#F5F5F6] px-5 py-3 uppercase bg-[#CB2B28] rounded-[0.4375rem]">
+                cartoon
+              </button>
+              <p className="flex gap-2 items-center uppercase text-black font-semibold">
+                <Lock size={20} className="text-gray-300" />
+                artistic
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-[#E3E4E7] bg-opacity-[26%] p-4 rounded-[0.5625rem] flex flex-col items-center gap-3">
+          <p className="uppercase text-[#CB2B28] text-[1.25rem] font-bold">
+            get inspired!
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className="uppercase text-black text-[1.25rem] font-medium">
+            best users designs
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <Image alt="Demon skull in a shape of a heart" src={skull} />
+          <div className="flex justify-between mx-8">
+            <input
+              className="rounded-[0.5625rem] bg-[#F5F5F6] px-2 shadow-md placeholder:text-[#C0C1C9]"
+              value="punk heart, gothic"
+            />
+            <button className="bg-[#CFCFD0] p-2 rounded-[0.4375rem] uppercase">
+              artistic
+            </button>
+          </div>
+          <button className="text-[#F5F5F6] px-14 py-2 uppercase bg-[#CB2B28] rounded-[0.4375rem]">
+            buy
+          </button>
+        </div>
+      </main>
+    </div>
+  );
 }
